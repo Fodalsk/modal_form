c3cf3a8ab98433334687eb696e283f6712aa2548
Amauri Edmundo Rauber Junior <amauri.rauber@gmail.com>
2021-03-30 22:57:55 +0100

    first upload

---
+++ js/functions.js
@@ -0,0 +1,27 @@
+$(function(){
+
+	abrirJanela();
+	verificarCliqueFechar();
+
+	function abrirJanela(){
+		$('.btn').click(function(e){
+			e.stopPropagation();
+			$('.bg').fadeIn();
+		})
+	}
+
+	function verificarCliqueFechar(){
+		var el = $('body,.closeBtn');
+
+		el.click(function(){
+			$('.bg').fadeOut();
+		})
+
+		$('.form').click(function(e){
+			e.stopPropagation();
+		})
+	}
+
+});
+
+
