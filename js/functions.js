cd0e0201bf5875b4e4b289167f997eedff8ddb84
Amauri Edmundo Rauber Junior <amauri.rauber@gmail.com>
2021-04-06 21:59:05 +0100

    character validation update

--- js/functions.js
+++ js/functions.js
@@ -1,5 +1,7 @@
 $(function(){
 
+	//Funções de abrir e fechar formulário
+
 	abrirJanela();
 	verificarCliqueFechar();
 
@@ -22,6 +24,85 @@ $(function(){
 		})
 	}
 
-});
+	//Eventos do formulário
 
+	$('input[type=text]').focus(function(){
+		resetarCampoInvalido($(this));
+
+	})
+
+	$('form#form1').submit(function(){
+		//e.preventDefault();
+		var nome = $('input[name=nome]').val();
+		var telefone = $('input[name=telefone]').val();
+		var email = $('input[name=email]').val();
+
+		if (verficarNome(nome) == false) {
+			aplicarCampoInvalido($('input[name=nome]'));
+		}else if(verificarTelefone(telefone) == false){
+			aplicarCampoInvalido($('input[name=telefone]'));
+		}else if(verificarEmail(email) == false){
+			aplicarCampoInvalido($('input[name=email]'));
+		}else{
+			alert('Formulário enviado com sucesso!');
+		}
+
+		return false;
+	})
+
+	//Funções para estilizar o campo do formulário
+
+	function aplicarCampoInvalido(el){
+		el.css('color','red');
+		el.css('border','2px solid red');
+		el.val('Campo inválido!');
+	}
+
+	function resetarCampoInvalido(el){
+		el.css('color','#363636');
+		el.css('border','1px solid #ccc');
+		el.val('');
+	}
+
+	//Funções para verificar nossos campos
+	function verficarNome(nome){
+		//contando a quantidade de espacos e os valores
+		if (nome == '') {
+			return false;
+		}
+		var amount = nome.split(' ').length;
+		var splitStr = nome.split(' ');
+		if (amount >= 2) {
+			for(var i = 0; i < amount; i++){
+				if(splitStr[i].match(/^[A-Z]{1}[a-z]{1,}$/)) {
+				}else{
+					return false;
+				}
+			}
+		}else{
+			return false;
+		}
+	}
+
+	function verificarTelefone(telefone){
+		if (telefone == '') {
+			return false;
+		}
+
+		if (telefone.match(/^\([0-9]{2}\)[0-9]{4}-[0-9]{4}$/) == null) {
+			return false;
+		}
+	}
+
+	function verificarEmail(email){
+		if (email == '') {
+			return false;
+		}
+
+		if (email.match(/^[a-z0-9-_.]{1,}+@+[a-z.]{1,}$/) == null) {
+			return false;
+		}
+	}
+
+});
 
